773e7f55f847af76d004e5852997501f1a3d54f2
Dorian Snyder <dastin1015@gmail.com>
2014-12-21 14:34:49 -0800

    m7spr: don't override build ID

--- cm.mk
+++ cm.mk
@@ -6,7 +6,6 @@ $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sprint_wwe \
-    BUILD_ID=KOT49H \
     BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/337390.3:user/release-keys" \
     PRIVATE_BUILD_DESC="5.03.651.3 CL337390 release-keys"
 
